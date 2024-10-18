049a8d526047bd8f82dc8cd9971aa8926b183dcc
umer <54628763+TheUmerator@users.noreply.github.com>
2024-10-18 07:22:22 +0500

    Create ONE div with DOM Manipulation

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.log('and were live')
 
+squareContainer=document.querySelector('.square-container');
 squareDiv=document.createElement('div');
 squareDiv.classList.add('square-div');
+
+squareContainer.appendChild(squareDiv);
+
+// We use a loop to create divs
+// Need a way to wrap divs after 16
+// Need to write hover code for the sketch divs
+// Need to cater to user input
