5613e4e7f577aacfbec9dbd2aa94bdd71e5fedeb
umer <54628763+TheUmerator@users.noreply.github.com>
2024-10-19 09:44:32 +0500

    create 16x16 grid and use mouseover event

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 console.log('and were live')
 
 squareContainer=document.querySelector('.square-container');
+// https://fonts.google.com/specimen/Cedarville+Cursive
+
+for(i=1;i<=256;i++){
 squareDiv=document.createElement('div');
 squareDiv.classList.add('square-div');
 
 squareContainer.appendChild(squareDiv);
 
-// We use a loop to create divs
-// Need a way to wrap divs after 16
-// Need to write hover code for the sketch divs
-// Need to cater to user input
+// squareDiv.addEventListener('mouseover',()=>{squareDiv.classList.add('hovered')})
+}
+
+const squareDivs=document.querySelectorAll('.square-div');
+
+squareDivs.forEach(squareDiv => {
+    squareDiv.addEventListener('mouseover',()=>{squareDiv.classList.add('hovered')})
+});
